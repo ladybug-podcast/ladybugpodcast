c95a89c77d0c5e31cf61e6d7dde19334063b4926
Kelly Vaughn <kelly@thetaproom.com>
2019-06-23 21:11:22 -0400

    add google tag manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,5 +74,15 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-tagmanager`,
+      options: {
+        id: "GTM-5JVF4M8",
+
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+      },
+    },
   ],
 }
