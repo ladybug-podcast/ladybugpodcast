9a7a748bcfff0e5c244d214b828185f449fe2087
Lindsey Kopacz <lkopacz@users.noreply.github.com>
2019-08-10 15:57:56 -0400

    CSS Pt 1.

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,6 +51,7 @@ module.exports = {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 590,
+              linkImagesToOriginal: false,
             },
           },
           {
